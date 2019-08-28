8dc6a29a9544b3137049936f6e6f98dcba70320b
xiaotiandada <952822399@qq.com>
2019-08-29 01:15:51 +0800

    下载图片

--- crawler/app.js
+++ crawler/app.js
@@ -1,10 +1,11 @@
 const superagent = require('superagent');
 const cheerio = require('cheerio')
+const fs = require('fs-extra')
+const path = require('path')
 
 let id = 1
 
 const getUrlHtml = async () => {
-  console.log('start')
   try {
     const res = await superagent.get(`https://www.zcool.com.cn/?p=${id}`);
     // console.log(res.text);
@@ -18,18 +19,46 @@ const getUrlHtml = async () => {
 const getDom = (html) => {
   if (!html) return
   const $ = cheerio.load(html)
+  let arr = []
   $('.all-work-list .work-list-box .card-box').each((i, el) => {
     let img = $(el).find('.card-img a img').attr('src')
     let title = $(el).find('.card-info .card-info-title a').text()
-    console.log(img, '--' , title)
+    arr.push({
+      title: title,
+      url: img
+    })
   })
-  console.log('end')
+  return arr
 }
 
+const downloadImg = async arr => {
+  try {
+    await fs.remove(path.join(__dirname, `/page${id}`))
+  } catch (error) {
+    console.log('删除文件夹失败')
+  }
+  try {
+    await fs.mkdir(path.join(__dirname, `/page${id}` ))
+  } catch (error) {
+    return console.log('创建文件夹失败')
+  }
+
+  const download = item => {
+    try {
+      const req =  superagent.get(item.url.replace('@260w_195h_1c_1e_1o_100sh.jpg', ''))
+      req.pipe(fs.createWriteStream(`./page${id}/${item.title}.png`))
+    } catch (error) {
+      return console.log(`下载图片失败${item.title}`, error)
+    }
+  }
+
+  for (const item of arr) await download(item)
+}
 
 const init = async () => {
   let urlHtml = await getUrlHtml()
-  await getDom(urlHtml)
+  let getDate = await getDom(urlHtml)
+  downloadImg(getDate)
 }
 
 init()
